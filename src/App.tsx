65fb8aa79779c0a51088c54f7b28ab05fed5edde
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-06-03 17:07:39 -0500

    Add chat functionality to main component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import "./App.css";
 import addBtn from "./assets/add-30.png";
 import saved from "./assets/bookmark.svg";
 import gptLogo from "./assets/chatgpt.svg";
+import gptImgLogo from "./assets/chatgptLogo.svg";
 import home from "./assets/home.svg";
 import msgIcon from "./assets/message.svg";
 import rocket from "./assets/rocket.svg";
+import sendBtn from "./assets/send.svg";
+import userIcon from "./assets/user-icon.png";
 
 function App() {
   return (
@@ -45,7 +48,34 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <div className="chats">
+          <div className="chat">
+            <img className="chatimg" src={userIcon} alt="" />
+            <p className="txt">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci assumenda quos dolorem mollitia expedita, hic dolorum ipsa obcaecati, soluta quasi ea accusantium in unde? Eum assumenda architecto beatae consequuntur omnis?
+            </p>
+          </div>
+          <div className="chat bot">
+            <img className="chatimg" src={gptImgLogo} alt="" />
+            <p className="txt">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eius, dolores deleniti voluptate aliquid excepturi libero inventore ipsum minus hic totam porro quaerat doloremque quas ut modi minima ea vitae facere aspernatur. Assumenda ipsam illum, porro, qui perferendis ad ipsum perspiciatis soluta dicta iste odit veritatis odio incidunt ratione, alias error reiciendis quis ex nobis praesentium! Nobis, fugit deleniti reprehenderit provident perspiciatis illo quam quos, dolores laudantium, necessitatibus vel obcaecati. Fuga exercitationem commodi ab totam quibusdam labore quidem, ex perspiciatis deleniti, velit dolore quam, neque ipsum praesentium earum. Nisi molestias quis, non odio quasi fugiat doloribus asperiores ipsa perferendis perspiciatis!
+            </p>
+          </div>
+        </div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input type="text" placeholder="Send a message" />
+            <button className="send">
+              <img src={sendBtn} alt="Send" />
+            </button>
+          </div>
+          <p>
+            AdnanGPT may produce inaccurate information about people, places, or
+            facts. AdnanGPT August 20 Version
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
