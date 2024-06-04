e77493b5cc54f05314861748a9aef83baacfda8d
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-06-03 23:48:59 -0500

    Commented out OpenAI code

--- src/openai.ts
+++ src/openai.ts
@@ -1,28 +1,28 @@
-import OpenAI from "openai";
+// import OpenAI from "openai";
 
-const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
+// const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 
-if (!OPENAI_API_KEY) {
-  throw new Error(
-    "The VITE_OPENAI_API_KEY environment variable is missing or empty; either provide it, or instantiate the OpenAI client with an apiKey option, like new OpenAI({ apiKey: 'My API Key' })."
-  );
-}
+// if (!OPENAI_API_KEY) {
+//   throw new Error(
+//     "The VITE_OPENAI_API_KEY environment variable is missing or empty; either provide it, or instantiate the OpenAI client with an apiKey option, like new OpenAI({ apiKey: 'My API Key' })."
+//   );
+// }
 
-const openai = new OpenAI({
-  apiKey: import.meta.env.OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
-});
+// const openai = new OpenAI({
+//   apiKey: import.meta.env.OPENAI_API_KEY,
+//   dangerouslyAllowBrowser: true,
+// });
 
-export async function sendMsgToOpenAI(message: string) {
-  const res = await openai.chat.completions.create({
-    messages: [{ role: "user", content: message }],
-    model: "gpt-3.5-turbo",
-    temperature: 0.7,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
+// export async function sendMsgToOpenAI(message: string) {
+//   const res = await openai.chat.completions.create({
+//     messages: [{ role: "user", content: message }],
+//     model: "gpt-3.5-turbo",
+//     temperature: 0.7,
+//     max_tokens: 256,
+//     top_p: 1,
+//     frequency_penalty: 0,
+//     presence_penalty: 0,
+//   });
 
-  return res.choices[0].message.content;
-}
+//   return res.choices[0].message.content;
+// }
